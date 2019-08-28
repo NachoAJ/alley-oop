f0c1cb3463c7b5f2a757bbf4ac4493a21d9063bb
NachoAJ <narchercher@gmail.com>
2019-08-28 10:53:31 +0200

    More maquetacion

--- client/src/components/Players-list.js
+++ client/src/components/Players-list.js
@@ -17,11 +17,32 @@ class PlayersList extends Component {
 				<button>FTM</button> */}
 				{this.props.playersInfo.map(elm => (
 					<div className='player-box' key={elm._id}>
-						<img src={elm.imageUrl} alt='Jugador'></img>
-						{elm.username}
-						<Link className='button' to={`/players/${elm._id}`}>
-							Ver Perfil
-						</Link>
+						<div className='container'>
+							<div className='row'>
+								<div className='col-md-4'>
+									<img src={elm.imageUrl} alt='Jugador'></img>
+								</div>
+								<div className='col-md-4'>
+									<h2>{elm.username}</h2>
+									<p className='capitalize'>Posición: {elm.position}</p>
+									<p>Edad: {elm.age}</p>
+								</div>
+								<div className='col-md-4'>
+									<div className='margin'>
+										<Link className='button' to={`/players/${elm._id}`}>
+											Ver Perfil
+										</Link>
+									</div>
+									<div className='stats'>
+										<span className='single-stat'>{elm.stats.ThreePM}</span>
+										<span className='single-stat'>{elm.stats.ThreePPCT}</span>
+										<br></br>
+										<span className='label'>3PM</span>
+										<span className='label'>3P%</span>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				))}
 			</div>
