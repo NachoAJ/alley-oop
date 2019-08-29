dab38522c616398e04915ef592747028fbcc5a4f
NachoAJ <narchercher@gmail.com>
2019-08-29 13:42:03 +0200

    Deploying

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+rm -rf server/public/; rm -rf client/build; cd client; npm run build-prod; cd ..; mv client/build/ server/public; git add .; git commit -m "Deploying"; git subtree push --prefix=server heroku master; heroku logs --tail
